6f5e31c84307005d70f834bc9d8591d91c786252
c4021820 <c4021820@my.shu.ac.uk>
2025-10-28 11:00:26 +0000

    colour picker

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-document.getElementById("myHeading").innerHTML ="Imaan's Test"
+const myHeading = document.getElementById("myHeading")
+myHeading.innerText = "Imaan"
+const firstLink = document.querySelector("nav ul li")
+firstLink.setAttribute("href", "http://www.google.co.uk");
+const colourButtons = document. querySelectorAll(".colPicker")
+console.dir(colourButtons)
+
+for (let i = 0; i < colourButtons.length; i++) {
+    colourButtons[i].style.backgroundColor = "green";
+}
+
+document.getElementById('myTestBtn').addEventListener('click', function() {
+    console.info('hi');
+  });
+
+document.querySelector(".red").addEventListener('click', function(ev){
+    document.body.setAttribute('class', "redBack");
+})
+
+document.querySelector(".green").addEventListener('click', function(ev){
+    document.body.setAttribute('class', "greenBack");
+})
+
+document.querySelector(".blue").addEventListener('click', function(ev){
+    document.body.setAttribute('class', "blueBack");
+})
+
+document.querySelector(".reset").addEventListener('click', function(ev){
+    document.body.removeAttribute('class', "currentPage");
+})
+
+
